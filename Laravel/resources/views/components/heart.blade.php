bc05abbad70c0fa38243f2bf5694b1a12f8004f4
RyuKyeongHo <azuretsuki@gmail.com>
2019-04-14 17:24:41 +0900

    while at work

--- Laravel/resources/views/components/heart.blade.php
+++ Laravel/resources/views/components/heart.blade.php
@@ -2,12 +2,12 @@
 <p class="view_text">{{$board->hits}}</p>
 
 @if(isset($board->hearts[0]) && $board->hearts[0]->heart == true)
-    {{-- <img src="{{asset('images/pic/like.png')}}" class="like heartToggle" id="{{$board->id}}"/> --}}
     <img src="{{asset('images/pic/like.png')}}" class="like" onclick="heartToggle({{$board->id}})" style="cursor:pointer"/>
-@else
-    {{-- <img src="{{asset('images/pic/like_1.png')}}" class="like heartToggle" id="{{$board->id}}"/> --}}
+@elseif(\Auth::check())
     <img src="{{asset('images/pic/like_1.png')}}" class="like" onclick="heartToggle({{$board->id}})" style="cursor:pointer"/>
+@else
+    <img src="{{asset('images/pic/like_1.png')}}" class="like"/>
 @endif
-
 <p class="like_text">{{$board->total_heart}}</p>
+
 <img src="{{asset('images/pic/download.png')}}" class="download"/>
