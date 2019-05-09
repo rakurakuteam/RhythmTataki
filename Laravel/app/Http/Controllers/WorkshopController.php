bd68ad94e667fabf1a633cb3db98ed367669dbc3
Ubuntu <ubuntu@ip-172-31-29-245.ap-northeast-2.compute.internal>
2019-05-09 19:05:54 +0900

    ec2-05-09

--- Laravel/app/Http/Controllers/WorkshopController.php
+++ Laravel/app/Http/Controllers/WorkshopController.php
@@ -31,27 +31,27 @@ class WorkshopController extends Controller
         $ts = $request->temporary_sound;
         $tss = explode('/', $ts);
         $audioName = $tss[count($tss)-1];
-        shell_exec("ffmpeg -i ".$ts." -c copy -ss ".$request->start_sec." -t ".$request->end_sec." -y /mnt/c/capstone/RhythmTataki/Laravel/public/song/clip/".$request->clip_name.".mp3");
-        Storage::disk('s3')->delete('temporarySound/'.$audioName);
+        shell_exec("ffmpeg -i ".$ts." -c copy -ss ".$request->start_sec." -t ".$request->end_sec." -y /var/www/capstone/RhythmTataki/Laravel/public/song/clip/".$request->clip_name.".ogg");
+        Storage::disk('s3')->delete('workshop/temporarySound/'.$audioName);
 
         $s3 = AwsFacade::createClient('s3');
         $s3->putObject([
             'Bucket' => 'capstone.rhythmtataki.bucket',
-            'Key' => 'workshop/drumSoundClip/'.$request->clip_name.'.ogg',
+            'Key' => 'workshop/drumSoundClip/'.\Auth::user()->email.'/'.$request->clip_name.'.ogg',
             'SourceFile' => 'song/clip/'.$request->clip_name.".ogg",
         ]);
 
-        $size = Storage::disk('s3')->size('workshop/drumSoundClip/'.$request->clip_name.'.ogg');
+        $size = Storage::disk('s3')->size('workshop/drumSoundClip/'.\Auth::user()->email.'/'.$request->clip_name.'.ogg');
 
         File::create([
            'user_id' => \Auth::user()->id,
-           'path' => Storage::disk('s3')->url('workshop/drumSoundClip/'),
+           'path' => Storage::disk('s3')->url('workshop/drumSoundClip/'.\Auth::user()->email.'/'),
            'name' => $request->clip_name.".ogg",
            'type' => "ogg",
            'size' => round($size/1000, 1),
         ]);
 
-        Shell_exec("rm /mnt/c/capstone/RhythmTataki/Laravel/public/song/clip/".$request->clip_name.".ogg");
+        Shell_exec("rm /var/www/capstone/RhythmTataki/Laravel/public/song/clip/".$request->clip_name.".ogg");
 
         // return response()->json($request->clip_name, 200, [], JSON_PRETTY_PRINT);
         return redirect('/workshop');
