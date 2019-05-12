37cb169c842f5a5b71cce98aa5c5a5adc7bcf4da
Ubuntu <ubuntu@ip-172-31-29-245.ap-northeast-2.compute.internal>
2019-05-12 23:22:08 +0900

    ec2-05-12

--- Laravel/app/Http/Controllers/UnityController.php
+++ Laravel/app/Http/Controllers/UnityController.php
@@ -164,9 +164,7 @@ class UnityController extends Controller
                 'dl_check' => true,
                 'created_at' => now(),
 	    ]);
-	    }
-
-        }   
+	}   
 
         $user_song_num = User_song::where('user_id', $user)->max('song_num')+1;
         
