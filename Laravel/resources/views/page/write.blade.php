fed6ab1b278ca8be637d0615fec7311e1d2a2efa
Ryu <azuretsuki@gmail.com>
2019-08-29 11:55:44 +0900

    08-29-login

--- Laravel/resources/views/page/write.blade.php
+++ Laravel/resources/views/page/write.blade.php
@@ -4,8 +4,8 @@
 @endsection
 
 @section('style')
-    {{-- <link rel="stylesheet" type="text/css" href="{{asset('css/header.css')}}" />
-    <link rel="stylesheet" type="text/css" href="{{asset('css/writeForm/write.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/header.css')}}" />
+    {{-- <link rel="stylesheet" type="text/css" href="{{asset('css/writeForm/write.css')}}" />
     <link rel="stylesheet" type="text/css" href="{{asset('css/footer.css')}}" />
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> --}}
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -28,4 +28,5 @@
 
 @push('scripts')
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> {{-- 부트스트랩 --}}
 @endpush
